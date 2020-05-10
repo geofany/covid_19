70483b77e095f6439c04b19b978fa7708e9acaef
Geofany Galindra <37582643+geofany@users.noreply.github.com>
2020-05-10 15:42:04 +0700

    informasi + bantuan

--- lib/bantuan.dart
+++ lib/bantuan.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class bantuan extends StatefulWidget {
   @override
@@ -10,7 +11,79 @@ class _bantuanState extends State<bantuan> {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Text("bantuan")
+        Stack(
+          children: <Widget>[
+            Container(
+              child: Image.asset("images/bg.jpg"),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 235),
+              
+              padding: EdgeInsets.fromLTRB(30, 50, 30, 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40))),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Pusat Bantuan",
+                      style: TextStyle(
+                          fontFamily: "WorkSans",
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Jika anda merasa memiliki gejala penyakit, silahkan hubungi kontak layanan di bawah ini :",
+                    style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 13,
+                        color: Colors.grey),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: Colors.white,
+                      splashColor: Colors.orange[600],
+                      focusColor: Colors.orange,
+                      padding: EdgeInsets.all(25),
+                      onPressed: () {
+                        launch("tel://911");
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "images/phone.png",
+                            height: 30,
+                          ),
+                          Text(
+                            "Info Layanan",
+                            style: TextStyle(
+                                fontFamily: "WorkSans",
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff46D39A)),
+                          ),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
       ],
     );
   }
--- lib/informasi.dart
+++ lib/informasi.dart
@@ -10,7 +10,127 @@ class _informasiState extends State<informasi> {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Text("Informasi")
+        Stack(
+          children: <Widget>[
+            Container(
+              child: Image.asset("images/bg.jpg"),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 235),
+              // height: 500,
+              padding: EdgeInsets.fromLTRB(30, 50, 30, 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40))),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Apa itu Coronavirus ?",
+                      style: TextStyle(
+                          fontFamily: "WorkSans",
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: Colors.white,
+                      splashColor: Colors.orange[600],
+                      focusColor: Colors.orange,
+                      padding: EdgeInsets.all(10),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "images/virus.png",
+                            height: 75,
+                          ),
+                          Text(
+                            "Mengenal Coronavirus",
+                            style: TextStyle(
+                                fontFamily: "WorkSans",
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff7195D7)),
+                          ),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: Colors.white,
+                      splashColor: Colors.orange[600],
+                      focusColor: Colors.orange,
+                      padding: EdgeInsets.all(10),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "images/info1.png",
+                            height: 75,
+                          ),
+                          Text(
+                            "Mencegah Coronavirus",
+                            style: TextStyle(
+                                fontFamily: "WorkSans",
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff7195D7)),
+                          ),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: Colors.white,
+                      splashColor: Colors.orange[600],
+                      focusColor: Colors.orange,
+                      padding: EdgeInsets.all(10),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "images/doctor-2.png",
+                            height: 75,
+                          ),
+                          Text(
+                            "Mengantisipasi",
+                            style: TextStyle(
+                                fontFamily: "WorkSans",
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff7195D7)),
+                          ),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
       ],
     );
   }
